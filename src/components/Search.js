4b6078f2dcce87b573492615658b493f94ed0ed8
bt <biantaobrad@gmail.com>
2020-04-26 00:23:37 +0800

    引入antd

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,49 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Input } from 'antd'
 
-const Search = (props) => {
-  const [searchValue, setSearchValue] = useState('')
+const { Search } = Input
 
-  const handleSearchInputChanges = e => {
-    setSearchValue(e.target.value)
-  }
+const SearchComp = (props) => {
+  // const [searchValue, setSearchValue] = useState('')
+  const searchFn = props.search
 
-  const callSearchFunction = e => {
-    e.preventDefault()
-    props.search(searchValue)
-    restInputFiled()
-  }
+  // const handleSearchInputChanges = e => {
+  //   setSearchValue(e.target.value)
+  // }
 
-  const restInputFiled = () => {
-    setSearchValue("")
+  // const callSearchFunction = e => {
+  //   e.preventDefault()
+  //   props.search(searchValue)
+  //   restInputFiled()
+  // }
+
+
+  // const restInputFiled = () => {
+  //   setSearchValue("")
+  // }
+
+
+
+  const handleSearch = value => {
+    searchFn(value)
+    // restInputFiled()
   }
 
   return (
-    <form>
-      <input type="text" value={searchValue} onChange={handleSearchInputChanges}/>
-      <input type="submit" value="SEARCH" onClick={callSearchFunction} />
-    </form>
+    // <form>
+    //   <input type="text" value={searchValue} onChange={handleSearchInputChanges}/>
+    //   <input type="submit" value="SEARCH" onClick={callSearchFunction} />
+    // </form>
+    <div style={{width: '500px', margin: '50px auto'}}>
+      <Search 
+        placeholder="input movie name"
+        enterButton="Search"
+        size="large"
+        onSearch={value => handleSearch(value)}
+      />
+    </div>
   )
 
 }
 
-export default Search
+export default SearchComp
